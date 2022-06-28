d714e787a9029ec5254b340b0de919adb78826b3
Karuppasamych <106671462+Karuppasamych@users.noreply.github.com>
2022-06-28 19:04:53 +0530

    Create DailyTask2.js

---
+++ DailyTask2.js
@@ -0,0 +1,22 @@
+
+
+
+1. Semantic tags
+
+// Ans : semantic tag are <Header>, <article>,<section>,<nav> <aside>,<footer>
+2. Iframe
+
+// Ans: Inline frame is used to embed the another document inside html
+3. Z index
+
+// Ans : it is used to bring the child element  over to the parent element .
+4. Canvas tag
+// Ans :  Canvas element is used to drawing graphics on the webpages. it has the several methods to draw like boxes, circle , text and adding images.
+5. Hoisting
+// Ans:  Hoisting is used to move the declared variables, classes , functions to top of the scope .
+6. let,var,const difference
+// Ans : let is a block scope, 
+        // var is a fucntion scope
+        // const variable is cannot modify it  
+7. Position
+// posistion property is used to keep the element positioned in document 
